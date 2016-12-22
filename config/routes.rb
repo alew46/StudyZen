36d24915fe86e05950527b314bab395186901945
Alex Lewis <alew46@gmail.com>
2016-12-22 14:10:15 -0500

    Added bootstrap and stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  root 'home#index'
+
+  resources :homes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
